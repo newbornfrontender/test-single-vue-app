3d9e031bd921c435f7eb620600d4275c494070f6
Infant Frontender <newbornfrontender@gmail.com>
2018-08-14 15:56:42 +0300

    Начал вести историю

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="container">
+    <Greeting class="component--first"/>
+    <Todo class="component--second"/>
+  </section>
+</template>
+
+<script>
+import Greeting from './components/Greeting.vue';
+import Todo from './components/Todo.vue';
+
+export default {
+  components: {
+    Greeting,
+    Todo,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #42b983;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-gap: 15px;
+  padding: 15px;
+}
+
+.component {
+  padding: 10px;
+  color: #fff;
+  background-color: #35495d;
+  border-radius: 10px;
+  box-sizing: border-box;
+}
+
+.title {
+  color: #fff;
+}
+</style>
---
+++ src/components/Greeting.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="component">
+    <h1 class="title title--main">{{ title }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Заголовок',
+    };
+  },
+};
+</script>
+
+<style>
+.title--main {
+  color: #42b983;
+}
+</style>
