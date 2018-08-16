a8742266427661c41e1c20cb99dc6ec1c8b9a9ae
Infant Frontender <newbornfrontender@gmail.com>
2018-08-16 11:13:32 +0300

    Добавил основной файл

---
+++ src/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import './assets/main.css';
+
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
